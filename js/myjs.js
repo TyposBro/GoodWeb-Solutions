852e325d0fad2789786c582294ed505a4e8b1d74
Umidjonov <umidjonov.azizbek@gmail.com>
2019-08-18 22:00:28 +0500

    <!--Info--> section is under construction

--- js/myjs.js
+++ js/myjs.js
@@ -1,11 +1,13 @@
-$('.nav-link').mouseenter(function(){
-    $(this).css("background-color", " #ff6600");
-    $(this).addClass("rounded-pill");
-  });
-  $('.nav-link').mouseleave(function(){
-      $(this).css("background-color", "");
-    });
+$('.nav-link').mouseenter(function() {
+  $(this).css("background-color", " #ff6600");
+  $(this).addClass("rounded-pill");
+});
+$('.nav-link').mouseleave(function() {
+  $(this).css("background-color", "");
+});
 
-$('div').unload(function(){
-  $(this).addClass("spinner-grow text-warning");
+$(document).ready(function() {
+  $('.active').css("background-color", " #ff6600");
+  $(".active").addClass("rounded-pill");
+  $('nav').css("background-color", "white");
 });
