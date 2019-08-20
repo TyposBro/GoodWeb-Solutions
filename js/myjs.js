cec843985a441bf140e25db9aeeca524811c5edb
Umidjonov <umidjonov.azizbek@gmail.com>
2019-08-20 07:21:00 +0500

    Info/Cards

--- js/myjs.js
+++ js/myjs.js
@@ -11,3 +11,8 @@ $(document).ready(function() {
   $(".active").addClass("rounded-pill");
   $('nav').css("background-color", "white");
 });
+
+
+
+// $('#about_01').click(function(){$(this).remove();},
+// function(){$(".container mt-3 col-md-6").html('<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe></div>');})
